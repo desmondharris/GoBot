1b09ed9614f4a6b51a9b93c430c889f97f5bc619
Desmond Harris <dkharr05@louisville.edu>
2024-10-29 17:56:46 -0400

    add js wrappers/initial commit

---
+++ assets/apiwrapper.js
@@ -0,0 +1,15 @@
+async function fetchAndLog(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    }
+}
+
+// Example usage:
+fetchAndLog('http://localhost:9090/user/1');
